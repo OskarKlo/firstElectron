ae30ed34f66b239404c31bc458d2ed379e491ee5
OskarKlo <oskark@thinkglobalschool.com>
2017-04-06 14:19:37 +0100

    Working Calculator

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ function switchText(){
     document.getElementById('important').innerHTML = "This is now important";
 }
 
+var userName = document.getElementById('userName');
+var passWord = document.getElementById('passWord');
+var signInBtn = document.getElementById('');
+
+function userInfo(){
+    console.log(userName);
+    console.log(passWord);
+    var passLength = length.passWord;
+    console.log(passLength);
+    // if (passLength > 6) {
+    //     console.log("Password is Good");
+    //  } else {
+    //     console.log("Your password is too short!");
+    //  }
+}
+
+// Calculator Practice
+// var textResults = document.getElementById('results');
+
+// function addNumbers(item){
+//     var results = item + 2;
+//     console.log(results);
+//     alert(results);
+//     textResults.innerHTML = results;
+// }
